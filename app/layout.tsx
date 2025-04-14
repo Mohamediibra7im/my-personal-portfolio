ec2712af0b83dd8e8e9629dca35bf9814676ac7a
Mohammed Ibrahim <HNU.24.308.Muhammad-Abdulnabi@hnu.edu.eg>
2025-04-14 20:00:53 +0200

    added new features to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import type React from "react"
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>{children}
-        <Analytics/>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
